e5c4d6b189a74152e403c18d9627c41b4ee05d40
Deepanshu Soni <117809462+deep3001@users.noreply.github.com>
2024-09-13 10:32:26 +0530

    cutomer support done

--- customer-support.php
+++ customer-support.php
@@ -1,4 +1,4 @@
-<?php include "components/phpComponents/phpcomponents.php";?>
+<?php include "components/phpComponents/phpcomponents.php"; ?>
 <!DOCTYPE html>
 <html dir="ltr">
 
@@ -11,7 +11,7 @@
     <meta name="description" content="Responsive, Highly Customizable Dashboard Template">
 
     <!-- Favicon -->
-    <?php 
+    <?php
     include "components/cssComponents/customer-support-css.php"
     ?>
 
@@ -52,7 +52,7 @@
                         <div class="w-auto px-0">
                             <div class="hp-header-logo d-flex align-items-center">
                                 <a href="overview" class="position-relative">
-                                   
+
 
                                     <img class="hp-logo hp-sidebar-visible hp-dark-none" src="app-assets/img/logo/logo-small-dark.png" alt="logo">
                                     <img class="hp-logo hp-sidebar-visible hp-dark-block" src="app-assets/img/logo/logo-small.png" alt="logo">
@@ -75,24 +75,24 @@
                     </div>
 
                     <?php
-  include "components/sidebar.php";
-  ?>
+                    include "components/sidebar.php";
+                    ?>
 
                 </div>
 
                 <?php
-  include "components/sideProfile.php";
-  ?>
+                include "components/sideProfile.php";
+                ?>
 
             </div>
         </div>
 
         <div class="hp-main-layout">
-        <?php
-        $title1="Support";
-        $title2="Ticket";
-        include "components/header.php";
-  ?>
+            <?php
+            $title1 = "Support";
+            $title2 = "Ticket";
+            include "components/header.php";
+            ?>
 
 
             <div class="offcanvas offcanvas-start hp-mobile-sidebar bg-black-20 hp-bg-dark-90" tabindex="-1" id="mobileMenu" aria-labelledby="mobileMenuLabel" style="width: 256px;">
@@ -110,7 +110,7 @@
                                 <img class="hp-logo hp-sidebar-visible hp-dark-block" src="app-assets/img/logo/logo-small.png" alt="logo">
                                 <img class="hp-logo hp-sidebar-hidden hp-dir-none hp-dark-none" src="app-assets/img/logo/LOGOFORWHITE.png" alt="logo">
                                 <img class="hp-logo hp-sidebar-hidden hp-dir-none hp-dark-block" src="app-assets/img/logo/LOGOFORBLACK.png" alt="logo">
-                                 <img class="hp-logo hp-sidebar-hidden hp-dir-block hp-dark-none" src="app-assets/img/logo/logo-rtl.svg" alt="logo">
+                                <img class="hp-logo hp-sidebar-hidden hp-dir-block hp-dark-none" src="app-assets/img/logo/logo-rtl.svg" alt="logo">
                                 <img class="hp-logo hp-sidebar-hidden hp-dir-block hp-dark-block" src="app-assets/img/logo/logo-rtl-dark.svg" alt="logo">
                             </a>
 
@@ -149,8 +149,8 @@
                                             <img class="hp-logo hp-sidebar-visible hp-dark-none" src="app-assets/img/logo/logo-small-dark.png" alt="logo">
                                             <img class="hp-logo hp-sidebar-visible hp-dark-block" src="app-assets/img/logo/logo-small.png" alt="logo">
                                             <img class="hp-logo hp-sidebar-hidden hp-dir-none hp-dark-none" src="app-assets/img/logo/LOGOFORWHITE.png" alt="logo">
-                                <img class="hp-logo hp-sidebar-hidden hp-dir-none hp-dark-block" src="app-assets/img/logo/LOGOFORBLACK.png" alt="logo">
-                                <img class="hp-logo hp-sidebar-hidden hp-dir-block hp-dark-none" src="app-assets/img/logo/logo-rtl.svg" alt="logo">
+                                            <img class="hp-logo hp-sidebar-hidden hp-dir-none hp-dark-block" src="app-assets/img/logo/LOGOFORBLACK.png" alt="logo">
+                                            <img class="hp-logo hp-sidebar-hidden hp-dir-block hp-dark-none" src="app-assets/img/logo/logo-rtl.svg" alt="logo">
                                             <img class="hp-logo hp-sidebar-hidden hp-dir-block hp-dark-block" src="app-assets/img/logo/logo-rtl-dark.svg" alt="logo">
                                         </a>
 
@@ -171,144 +171,166 @@
                             </div>
 
                             <?php
-  include "components/sidebar.php";
-  ?>
+                            include "components/sidebar.php";
+                            ?>
 
                         </div>
 
                         <?php
-  include "components/sideProfile.php";
-  ?>
+                        include "components/sideProfile.php";
+                        ?>
 
                     </div>
                 </div>
             </div>
 
             <div class="hp-main-layout-content">
-            <div class="row mb-32 gy-32">
-                    <!-- custom form -->
-                    <div class="col-12 col-md-12">
-                        <div class="card custom-card">
-                            <div class="card-body">
-                                <h3 class="card-title mb-32">Submit a Ticket</h3>
-                                <form>
-                                    <div class="mb-24 mt-15">
-                                        <label for="mt5group" class="form-label">Ticket Type</label>
-                                        <select class="form-select" id="mt5group" aria-describedby="mt5groupHelp">
-                                            <option value="" selected>Please Select</option>
-                                            <option value="Orion//Demo">Orion//</option>
-                                        </select>
-                                    </div>
-                                    <div class="mb-24">
-                                        <label for="leverage" class="form-label">Subject</label>
-                                        <input type="text" name="subject" value="" class="form-control">
-                                    </div>
-                                    <div class="mb-24">
-                                        <label for="leverage" class="form-label">Message</label>
-                                        <textarea rows="" cols="5" type="text" name="message" value="" class="form-control"></textarea> 
-                                    </div>
-                                    <div class="mb-24">
-                                        <label for="leverage" class="form-label">Attachment (MAXIMUM SIZE: 10 MB)</label>
-                                        <input
-                                        type="file"
-                                        class="form-control"
-                                        id="idFront1"
-                                        aria-describedby="idFrontHelp1"
-                                        accept="image/*"
-                                    /></div>
-                                    <div class="d-flex justify-content-end">
-                                        <button class="pushable">
-                                            <span class="shadow"></span>
-                                            <span class="edge"></span>
-                                            <span class="front">Submit</span>
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <!-- Support Tickets -->
+                <div class="row mb-32 gy-32">
                     <div class="col-12">
-                        <div class="card custom-card">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between align-items-center flex-wrap mb-3">
-                                    <h3 class="card-title mb-15">Customer Support Details</h3>
-                                    <div class="d-flex flex-wrap align-items-center">
-                                        <button class="btn btn-secondary me-2 mb-2 mb-md-0 btn-sm">
-                                            <span class="bi bi-download"></span>&nbsp;&nbsp;Export to Excel</button>
-                                        <select class="form-select mb-2 mb-md-0" style="width: auto;">
-                                            <option selected>Show Entries</option>
-                                            <option value="10">10</option>
-                                            <option value="25">25</option>
-                                            <option value="50">50</option>
-                                            <option value="100">100</option>
-                                        </select>
+                        <div class="hp-bg-black-bg py-32 py-sm-64 px-24 px-sm-48 px-md-80 position-relative overflow-hidden hp-page-content" style="border-radius: 32px;">
+                            <svg width="358" height="336" fill="none" xmlns="http://www.w3.org/2000/svg" class="position-absolute hp-rtl-scale-x-n1" style="bottom: 0px; right: 0px;">
+                                <path d="M730.404 135.471 369.675-6.641l88.802 164.001-243.179-98.8 246.364 263.281-329.128-126.619 114.698 166.726-241.68-62.446" stroke="url(#a)" stroke-width="40" stroke-linejoin="bevel"></path>
+                                <defs>
+                                    <lineargradient id="a" x1="315.467" y1="6.875" x2="397.957" y2="337.724" gradientunits="userSpaceOnUse">
+                                        <stop stop-color="#fff"></stop>
+                                        <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                                    </lineargradient>
+                                </defs>
+                            </svg>
+
+                            <div class="row">
+                                <div class="col-sm-6 col-12">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <h1 class="mb-0 hp-text-color-black-0"> Customer Support </h1>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <ol class="breadcrumb mt-24">
+                                                <li class="breadcrumb-item breadcrumb-link">
+                                                    <a href="/"> Home </a>
+                                                </li>
+
+
+                                                <li class="breadcrumb-item">
+                                                    <a href="javascript:;">
+                                                        <span class="hp-text-color-black-0"> Customer Support </span>
+                                                    </a>
+                                                </li>
+
+
+
+
+                                            </ol>
+                                        </div>
+
+
                                     </div>
-                                </div>
-                                <div class="mb-24"></div>
-                                <div class="mb-24"></div>
-                                <div class="table-responsive">
-                                    <table class="table table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col" style="min-width: 150px;">TICKET ID</th>
-                                                <th scope="col" style="min-width: 170px;">STATUS</th>
-                                                <th scope="col" style="min-width: 120px;">SUBJECT</th>
-                                                <th scope="col" style="min-width: 120px;">UPDATED</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody id="tableBody">
-                                            <tr>
-                                                <td>01-01-2023</td>
-                                                <td>123456</td>
-                                                <td>Orion</td>
-                                                <td>100</td>
-                                            </tr>
-                                            <tr>
-                                                <td>01-01-2023</td>
-                                                <td>123456</td>
-                                                <td>Orion</td>
-                                                <td>100</td>
-                                            </tr>
-                                            <tr>
-                                                <td>01-01-2023</td>
-                                                <td>123456</td>
-                                                <td>Orion</td>
-                                                <td>100</td>
-                                            </tr>
-                                            <tr>
-                                                <td>01-01-2023</td>
-                                                <td>123456</td>
-                                                <td>Orion</td>
-                                                <td>100</td>
-                                            </tr>
-                                            <!-- Add more rows as needed -->
-                                        </tbody>
-                                    </table>
                                 </div>
                             </div>
                         </div>
                     </div>
-            </div>
+
+                    <div class="col-12">
+                        <div class="row g-32">
+
+
+
+
+
+
+                            <div class="col-12 col-xl-4">
+                                <div class="card hp-card-6">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-12 text-center">
+                                                <img src="app-assets/img/customer-service.png" style="max-width: 200px;" alt="Buy Pro Account to explore Premium Features" class="hp-dark-none">
+                                                <img src="app-assets/img/customer-service.png" style="max-width: 200px;" alt="Buy Pro Account to explore Premium Features" class="hp-dark-block mx-auto">
+
+                                                <h4 class="my-24 me-4 fw-bold">Send a Whatsapp Message  or Call to our team</h4>
+                                                <h6 class="my-24 me-4 fw-bold">+917610750147</h6>
+
+
+                                                <a href="https://wa.me/917610750146" target="_blank" class="btn btn-primary">
+                                                    <span>Message NOW</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-xl-4">
+                                <div class="card hp-card-6">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-12 text-center">
+                                                <img src="app-assets/img/whatsapp.png" style="max-width: 200px;" alt="Buy Pro Account to explore Premium Features" class="hp-dark-none">
+                                                <img src="app-assets/img/whatsapp.png" style="max-width: 200px;" alt="Buy Pro Account to explore Premium Features" class="hp-dark-block mx-auto">
+
+                                                <h4 class="my-24 me-4 fw-bold">Send a Whatsapp Message to Your Sponsorer</h4>
+                                                <h6 class="my-24 me-4 fw-bold">+917610750147</h6>
+
+                                                <a href="https://wa.me/917610750146" target="_blank" class="btn btn-primary">
+                                                    <span>Message NOW</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                          
+
+
+
+                            <div class="col-12 col-xl-4">
+                                <div class="card hp-card-6">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-12 text-center">
+                                                <img src="app-assets/img/mail.png" alt="Upgrade Account" style="max-width: 200px;" class="hp-dark-none">
+                                                <img src="app-assets/img/mail.png" style="max-width: 200px;" alt="Upgrade Account" class="hp-dark-block mx-auto">
+
+                                                <form class="mt-36" action="https://mail.google.com/mail/u/0/#inbox?compose=new" method="get" target="_blank">
+                                                    <input type="hidden" name="to" value="support@himallaya.com">
+                                                    <div class="mb-16">
+                                                        <label for="subscribeEmail" class="w-100 text-start form-label">
+                                                            <span class="text-danger me-8">*</span>Email: support@himallaya.com
+                                                        </label>
+                                                    </div>
+                                                    <button type="submit" class="btn btn-primary"  >Send Email</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+
+
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <?php
-  include "components/footer.php";
-  ?>
+            include "components/footer.php";
+            ?>
 
-        
+
         </div>
     </main>
 
     <?php
-  include "components/appTheme.php";
-  ?>
+    include "components/appTheme.php";
+    ?>
 
 
     <a href="https://themeforest.net/item/yoda-react-admin-template-react-hooks-redux-toolkit-ant-design/33791048" target="_blank" class="hp-buy-now-btn position-fixed">
-        
+
     </a>
 
     <div class="scroll-to-top">
@@ -323,9 +345,9 @@
     </div>
 
     <!-- Plugin -->
-  <?php
-  include  "components/jsComponents/customer-support-js.php"
-  ?>
+    <?php
+    include  "components/jsComponents/customer-support-js.php"
+    ?>
 </body>
 
 </html>
